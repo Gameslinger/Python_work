3b125bb08e9a35f7be264ddc9b823cd1d39af804
Gameslinger <gabriel.maxfield@apastudents.org>
2015-06-25 12:45:41 -0600

    Create Python_test.py

---
+++ Python_test.py
@@ -0,0 +1,20 @@
+#first Python program!
+import time
+def start():
+    first = raw_input("Hello whats your name?\n    ")
+
+    second = raw_input('Hello '+first+", do you like python(yes/no)\n    ")
+
+    if (second.lower() == 'yes'):
+        print "you know it!"
+        print '_____\n|0 -|\n| - |'
+    else:
+        print "good bye!"
+        time.sleep(0.5)
+        i = 1
+        while i < 31:
+            print "You need help!"+str(i)
+            i +=1
+start()
+
+
